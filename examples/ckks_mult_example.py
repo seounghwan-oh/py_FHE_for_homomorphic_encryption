7352ad8045d252764275f0a5c4f7a609725bc6ef
sh <shwan@f1security.co.kr>
2021-09-03 15:06:20 +0900

    Random number generation

--- examples/ckks_mult_example.py
+++ examples/ckks_mult_example.py
@@ -26,55 +26,63 @@ def main():
     decryptor = CKKSDecryptor(params, secret_key)
     evaluator = CKKSEvaluator(params)
 
-    # write
-    message1 = [11000, 1, 1, 1]
-    message2 = [0.05, 1, 1, 1]
-    message3 = [5, 1, 1, 1]
-
-    plain1 = encoder.encode(message1, scaling_factor)
-    plain2 = encoder.encode(message2, scaling_factor)
-    plain3 = encoder.encode(message3, scaling_factor)
-    ciph1 = encryptor.encrypt(plain1)
-    ciph2 = encryptor.encrypt(plain2)
-
-    ciph_prod1 = evaluator.multiply(ciph1, ciph1, relin_key)
-    for _ in range(3):
-        ciph_prod1 = evaluator.multiply(ciph_prod1, ciph1, relin_key)
-
-    ciph_prod2 = evaluator.multiply(ciph2, ciph2, relin_key)
-    for _ in range(2):
-        ciph_prod2 = evaluator.multiply(ciph_prod2, ciph2, relin_key)
-
-    ciph_prod = evaluator.multiply(ciph_prod1, ciph_prod2, relin_key)
-    result = evaluator.multiply_plain(ciph_prod, plain3)
-
-    decrypted_prod = decryptor.decrypt(result)
-    decoded_prod = encoder.decode(decrypted_prod)
-
-    # print(decoded_prod)
+    # complete
     file = open("ckks_mult_example.txt", "w")
+    for _ in range(25000):
+        message1 = []
+        message2 = []
+        message3 = []
+        for _ in range(4):
+            num1 = uniform(1000, 10000)
+            num2 = uniform(0.001, 0.01)
+            message1.append(complex(num1, 0))
+            message2.append(complex(num2, 0))
+            message3.append(5)
+        # print(message1, message2)
 
-    for i in range(len(message1)):
-        A1 = message1[i]
-        A5 = A1 ** 5
-        B1 = message2[i]
-        B4 = B1 ** 4
+        plain1 = encoder.encode(message1, scaling_factor)
+        plain2 = encoder.encode(message2, scaling_factor)
+        plain3 = encoder.encode(message3, scaling_factor)
+        ciph1 = encryptor.encrypt(plain1)
+        ciph2 = encryptor.encrypt(plain2)
 
-        origin_result = abs(complex(5 * A5 * B4, 0))
-        ckks_result = abs(decoded_prod[i])
-        operation_result = ((origin_result - ckks_result) / (origin_result))
-        percent_result = abs(complex(0.0001, 0))
+        ciph_prod1 = evaluator.multiply(ciph1, ciph1, relin_key)
+        for _ in range(3):
+            ciph_prod1 = evaluator.multiply(ciph_prod1, ciph1, relin_key)
 
-        data = "A^5:" + str(A5) + " " + "B^4:" + str(B4) + " " + "CKKSresult:" + str(
-            ckks_result) + " " + "Originresult:" + str(origin_result)
-        if operation_result < percent_result:
-            # print("True")
-            result_data = data + " " + "result < 0.01%:True"
-            file.write(result_data + "\n")
-        else:
-            # print("False")
-            result_data = data + " " + "result < 0.01%:False"
-            file.write(result_data + "\n")
+        ciph_prod2 = evaluator.multiply(ciph2, ciph2, relin_key)
+        for _ in range(2):
+            ciph_prod2 = evaluator.multiply(ciph_prod2, ciph2, relin_key)
+
+        ciph_prod = evaluator.multiply(ciph_prod1, ciph_prod2, relin_key)
+        result = evaluator.multiply_plain(ciph_prod, plain3)
+
+        decrypted_prod = decryptor.decrypt(result)
+        decoded_prod = encoder.decode(decrypted_prod)
+
+        # print(decoded_prod)
+
+        for i in range(len(message1)):
+            A1 = message1[i]
+            A5 = A1 ** 5
+            B1 = message2[i]
+            B4 = B1 ** 4
+
+            origin_result = abs(complex(5 * A5 * B4, 0))
+            ckks_result = abs(decoded_prod[i])
+            operation_result = ((origin_result - ckks_result) / (origin_result))
+            percent_result = abs(complex(0.0001, 0))
+
+            data = "A^5:" + str(A5) + " " + "B^4:" + str(B4) + " " + "CKKSresult:" + str(
+                ckks_result) + " " + "Originresult:" + str(origin_result)
+            if operation_result < percent_result:
+                # print("True")
+                result_data = data + " " + "result < 0.01%:True"
+                file.write(result_data + "\n")
+            else:
+                # print("False")
+                result_data = data + " " + "result < 0.01%:False"
+                file.write(result_data + "\n")
 
     file.close()
     print("Complete")
